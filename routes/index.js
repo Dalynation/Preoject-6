7ffe4bc12df71c3ab4db37174eaae9f37ac61b10
Dalynation <42184269+Dalynation@users.noreply.github.com>
2018-12-29 09:12:07 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log("World");
-  next();
-});
+//-------------------------
+const app = express();
+
+app.use('/static', express.static('public')); //this middleware is where im expressing that my site uses static and public routes
+
+app.set('view engine', 'pug'); //this is where I chosen to run the view engine on my server, and have also read pug files
+//-------------------------
+
+//----
+
+// router.use((req, res, next) => {
+//   console.log("World");
+//   next();
+// });
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index'); // This render my index page
 });
 
 router.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about'); // This renders my about page
 });
 
-router.get('/project', (req, res) => {
-  res.render('project', { prompt: "Here's my projects!"});
-});
+//----
 
-module.exports = router;
+module.exports = router; // this'll help me require the needed modules as requested
