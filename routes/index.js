63f49ce85f11fdca218c972e870b9cf7e3664cd9
Dalynation <42184269+Dalynation@users.noreply.github.com>
2018-12-29 09:43:40 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ app.use('/static', express.static('public')); //this middleware is where im expr
 app.set('view engine', 'pug'); //this is where I chosen to run the view engine on my server, and have also read pug files
 //-------------------------
 
-//----
+const { projects } = require('./../data.json'); // This reqires the data I have store in my Json file
+
 
 // router.use((req, res, next) => {
 //   console.log("World");
@@ -17,13 +18,18 @@ app.set('view engine', 'pug'); //this is where I chosen to run the view engine o
 // });
 
 router.get('/', (req, res) => {
-  res.render('index'); // This render my index page
+  res.render('index',{ projects }); // This render my index page
 });
 
 router.get('/about', (req, res) => {
   res.render('about'); // This renders my about page
 });
 
-//----
+router.get('/project/:id', (req, res) => {
+  const { id } = req.params; // based on the id chosen the specific data for it will be retreived
+  const projectData = projects[id];
+  res.render('project', { projectData }); // this renders my project page with the project data
+});
+
 
 module.exports = router; // this'll help me require the needed modules as requested
